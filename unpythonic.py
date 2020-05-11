0c63a5cef284133d04c1a9460c30866d8e521973
Derrick Eckardt <derrick.eckardt@gmail.com>
2020-05-11 10:12:22 +0000

    i was wrong

--- unpythonic.py
+++ unpythonic.py
@@ -34,8 +34,8 @@ from random import randint
 dict1 ={}
 dict2 = {}
 for i in range(1000):
-    dict1[i] = randint(0,255)
-    dict2[i] = randint(0,255)
+    dict1[i] = randint(1,16)
+    dict2[i] = randint(1,16)
 
 # Dictionary lookup
 def method2(dict1,dict2):
@@ -62,6 +62,21 @@ def abs3(dict1,dict2):
         for j in range(1000):
             abs_value = ((dict1[i]-dict2[j])**2)**(0.5)
 
+
+def get_manhattan(current, goal):
+    distance_x = (goal-1)%4 - (current-1)%4
+    # distance_y = (goal-1)//4 - (current-1)//4
+    d_man_x = 1 if distance_x == 3 or distance_x == -3 else distance_x if distance_x >= 0 else distance_x*-1
+    # d_man_y = 1 if distance_y == 3 or distance_y == -3 else distance_y if distance_y >= 0 else distance_y*-1
+    # return (d_man_x**(0.91) + d_man_y**(0.91))**(1.414)
+    return d_man_x
+    
+def abs4(dict1,dict2):
+    for i in range(1000):
+        for j in range(1000):
+            abs_value = get_manhattan(dict1[i],dict2[j])
+    
+    
 """
 
 t = timeit.Timer(stmt="method1(dict1,dict2)", setup=setup1)
@@ -78,6 +93,9 @@ t = timeit.Timer(stmt="abs2(dict1,dict2)", setup=setup2)
 print(t.timeit(1))
 
 t = timeit.Timer(stmt="abs3(dict1,dict2)", setup=setup2)
+print(t.timeit(1))
+
+t = timeit.Timer(stmt="abs4(dict1,dict2)", setup=setup2)
 print(t.timeit(1))
 
 
