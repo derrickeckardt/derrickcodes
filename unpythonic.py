823b8b6936206f9d46913ce6bf58265454bb3e6c
Derrick Eckardt <derrick.eckardt@gmail.com>
2020-05-12 23:37:06 +0000

    some progress

--- unpythonic.py
+++ unpythonic.py
@@ -56,6 +56,13 @@ def abs2(dict1,dict2):
         for j in range(1000):
             abs_value = dict1[i]-dict2[j] if dict1[i] >= dict2[j] else dict2[j] - dict1[i]
 
+def abs2_a(dict1,dict2):
+    for i in range(1000):
+        for j in range(1000):
+            delta_x = dict1[i]-dict2[j]
+            abs_value = delta_x if delta_x >= 0 else delta_x*-1
+
+
 
 def abs3(dict1,dict2):
     for i in range(1000):
@@ -70,33 +77,56 @@ def get_manhattan(current, goal):
     # d_man_y = 1 if distance_y == 3 or distance_y == -3 else distance_y if distance_y >= 0 else distance_y*-1
     # return (d_man_x**(0.91) + d_man_y**(0.91))**(1.414)
     return d_man_x
+
+def get_manhattan_2(current, goal):
+    distance_x = (goal-1)%4 - (current-1)%4
+    # distance_y = (goal-1)//4 - (current-1)//4
+    d_man_x = 1 if distance_x == 3 or distance_x == -3 else abs(distance_x)
+    
+    # d_man_y = 1 if distance_y == 3 or distance_y == -3 else distance_y if distance_y >= 0 else distance_y*-1
+    # return (d_man_x**(0.91) + d_man_y**(0.91))**(1.414)
+    return d_man_x
+    
+
     
 def abs4(dict1,dict2):
     for i in range(1000):
         for j in range(1000):
             abs_value = get_manhattan(dict1[i],dict2[j])
     
+def abs5(dict1,dict2):
+    for i in range(1000):
+        for j in range(1000):
+            abs_value = get_manhattan_2(dict1[i],dict2[j])
+    
     
 """
 
 t = timeit.Timer(stmt="method1(dict1,dict2)", setup=setup1)
-print(t.timeit(1))
+print("method1",t.timeit(1))
 
 t = timeit.Timer(stmt="method2(dict1,dict2)", setup=setup2)
-print(t.timeit(1))
+print("method2",t.timeit(1))
 
 
 t = timeit.Timer(stmt="abs1(dict1,dict2)", setup=setup1)
-print(t.timeit(1))
+print("abs1",t.timeit(1))
 
 t = timeit.Timer(stmt="abs2(dict1,dict2)", setup=setup2)
-print(t.timeit(1))
+print("abs2",t.timeit(1))
+
+t = timeit.Timer(stmt="abs2_a(dict1,dict2)", setup=setup2)
+print("abs2_a",t.timeit(1))
+
 
 t = timeit.Timer(stmt="abs3(dict1,dict2)", setup=setup2)
-print(t.timeit(1))
+print("abs3",t.timeit(1))
 
 t = timeit.Timer(stmt="abs4(dict1,dict2)", setup=setup2)
-print(t.timeit(1))
+print("abs4",t.timeit(1))
+
+t = timeit.Timer(stmt="abs5(dict1,dict2)", setup=setup2)
+print("abs5",t.timeit(1))
 
 
 
